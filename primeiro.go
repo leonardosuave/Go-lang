78fb01b3f654ee8821bddab7107a25da72eb88a1
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-28 16:49:51 -0300

    first cod with Golang

---
+++ primeiro.go
@@ -0,0 +1,13 @@
+package main //significa que oque será escrito está dentro do arquivo main.
+import "fmt"
+
+/*//Importa bibliotecas. Pular apenas a linha para add mais bibliotecas. (neste caso ja possuo fmt por extensão).
+
+import (
+	"fmt"
+
+)*/
+
+func main() {
+	fmt.Println("Olá mundo!")
+}
